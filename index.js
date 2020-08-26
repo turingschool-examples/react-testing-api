6dd4b64117dcfa6affce4df9f4f9fa650e2cb4cc
Khalid Williams <khalidwill27@gmail.com>
2020-08-25 21:59:39 -0600

    Add post route

--- index.js
+++ index.js
@@ -23,5 +23,25 @@ app.get('/contacts', (req, res) => {
   res.status(200).json({contacts: app.locals.contacts});
 })
 
+app.post('/contacts', (req, res) => {
+  const requiredProperties = [ "name", "number" ];
+  //console.log(req)
+  const receivedProperties = Object.keys(req.body);
+  console.log(receivedProperties);
+  for (let property of requiredProperties) {
+    if (! receivedProperties.includes(property)) {
+      return res.status(422).json({message: `Cannot POST: missing property ${property} in response.`});
+    }
+  }
+  const newContact = {
+    ...req.body,
+    id: Date.now(),
+    bestFriend: false
+  }
+
+  app.locals.contacts.push(newContact);
+  return res.status(201).json({newContact: newContact});
+});
+
 
 app.listen(8080, () => console.log('App listening on port 8080'));
